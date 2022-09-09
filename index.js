95ddd3fd411945ddac4a640b5087fe3cf0b87eae
Grayce Muthui <graycemuthui20@gmail.com>
2022-09-09 17:26:29 +0300

    Fix the hamburger menu

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-const navContainer = document.getElementById("navContainer");
-const openBtn = document.getElementById("openBtn");
-const closeBtn = document.getElementById("closeBtn");
-const closeMenu = document.getElementById("navLinks");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-openBtn.onclick = function openBtn() {
-  navContainer.style.right = " 0";
-  console.log("clicked");
-};
-
-closeBtn.onclick = function closeBtn() {
-  navContainer.style.right = "-200px";
-  console.log("clicked");
-};
-
-closeMenu.onclick = function closeBtn() {
-  navContainer.style.right = "-200px";
-  console.log("clicked");
-};
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
